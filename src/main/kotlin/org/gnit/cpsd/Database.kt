f47b8a24f7b6ee0357622527a3906c811816d260
Hokuto Ide <nehemiaharchive@gmail.com>
2021-05-18 14:48:20 +0900

    Add database connection test

--- src/main/kotlin/org/gnit/cpsd/Database.kt
+++ src/main/kotlin/org/gnit/cpsd/Database.kt
@@ -3,9 +3,14 @@ package org.gnit.cpsd
 import org.neo4j.driver.AuthTokens
 import org.neo4j.driver.Driver
 import org.neo4j.driver.GraphDatabase
-import java.lang.RuntimeException
 import java.util.Properties
 
+/**
+ * This function reads db.properties file to obtain username and password of Neo4j Database.
+ * db.properties example:
+ * username=xxx
+ * password=xxx
+ */
 fun getDriver(): Driver {
     val props = Properties()
     Thread.currentThread().contextClassLoader.getResourceAsStream("db.properties")
@@ -15,4 +20,16 @@ fun getDriver(): Driver {
         "bolt://localhost:7687",
         AuthTokens.basic(props.getProperty("username"), props.getProperty("password"))
     ) ?: throw RuntimeException("Driver is null")
+}
+
+/**
+ * Database connection test
+ */
+fun main() {
+    val driver = getDriver()
+    val session = driver.session()
+    val churches = session.run("MATCH (c:Church) RETURN c;")
+    println(churches.list().size)
+    session.close()
+    driver.close()
 }
